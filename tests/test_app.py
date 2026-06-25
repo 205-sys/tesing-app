07ed3570c999e31c7ff1297007824e21c0b76cc8
205-sys <bunsengsan205@gmail.com>
2026-06-25 13:31:13 +0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,21 @@
 from src.app import create_app
 
+
 def test_home():
     app = create_app()
     client = app.test_client()
-    res = client.get("/")
-    assert res.status_code == 200
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json["message"] == "Hello from CI/CD Demo"
+
 
 def test_health():
     app = create_app()
     client = app.test_client()
-    res = client.get("/health")
-    assert res.json["status"] == "healthy"
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
